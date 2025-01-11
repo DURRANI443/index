eb44a58bc46664951a9e38f4a594a1d5d1cf21ec
DURRANI443 <abdulwahabkhandurrani073@gmail.com>
2025-01-11 15:11:50 +0500

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,31 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Collect the form data
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    // Email configuration
+    $to = "tusharmalhi02@gmail.com"; // Replace with your email address
+    $subject = "New Contact Form Submission from $name";
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+    // Email body
+    $body = "Name: $name\n";
+    $body .= "Email: $email\n\n";
+    $body .= "Message:\n$message\n";
+
+    // Send the email
+    if (mail($to, $subject, $body, $headers)) {
+        echo "<p>Thank you for contacting us, $name. We will get back to you shortly!</p>";
+    } else {
+        echo "<p>Sorry, there was an error sending your message. Please try again later.</p>";
+    }
+} else {
+    echo "<p>Invalid request method.</p>";
+}
+
+?>
